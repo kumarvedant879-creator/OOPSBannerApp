63e2d2d3e7adb99587b9f6f37a98e91a2f5bb346
kumarvedant879-creator <kumarvedant879@gmail.com>
2026-02-17 00:00:20 +0530

    Coded to Render OOPS as Banner using Print Statements

---
+++ OOPSBanner.java
@@ -0,0 +1,13 @@
+public class OOPSBanner
+{
+	public static void main()
+	{
+		System.out.println("  *****  "+" "+"  *****  "+" "+" ******* "+" "+" ********");
+		System.out.println(" ******* "+" "+" ******* "+" "+"**     **"+" "+"**     **");
+		System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+		System.out.println("**     **"+" "+"**     **"+" "+"******** "+" "+" ******* ");
+		System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"       **");
+		System.out.println(" ******* "+" "+" ******* "+" "+"**       "+" "+"**     **");
+		System.out.println("  *****  "+" "+"  *****  "+" "+"**       "+" "+"******** ");
+	}
+}
