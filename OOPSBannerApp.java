1d26c0e62489c8d39b1cd512c5da8c4d786f2488
kumarvedant879-creator <kumarvedant879@gmail.com>
2026-03-11 09:13:56 +0530

    Added UC3 OOPS Banner using String.join()

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        printLine("  *****  ", "  *****  ", " ******  ", " ****** ");
+        printLine(" **   ** ", " **   ** ", " **   ** ", "**   ** ");
+        printLine("**     **", "**     **", " **   ** ", "**      ");
+        printLine("**     **", "**     **", " ******  ", " *****  ");
+        printLine("**     **", "**     **", " **      ", "      **");
+        printLine(" **   ** ", " **   ** ", " **      ", "**   ** ");
+        printLine("  *****  ", "  *****  ", " **      ", " *****  ");
+    }
+
+    public static void printLine(String... parts) {
+        System.out.println(String.join("   ", parts));
+    }
+}
